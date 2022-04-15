451e4d07ee37efa53805fccc9bbf77763ae866c4
Thomas Koman <99066789@mydavinci.nl>
2022-04-15 14:32:00 +0200

    registration program

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-listlist = ['a', 'b']
-if listlist[2]:
-    print('hey')
-else:
-    print("no")
+from random import choice, randint
+from string import ascii_lowercase
+
+print(' '.join(
+    ['a'
+    if randint(0, 1) == 0 
+    else choice(ascii_lowercase)  
+    for i in range(32)]))
